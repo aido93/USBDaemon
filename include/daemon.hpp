2133907db0db8c513c76b516fbe7a6966e0aa50b
aido93 <aidos.tanatos@gmail.com>
2015-09-08 22:01:13 +0400

    Update daemon.hpp

--- include/daemon.hpp
+++ include/daemon.hpp
@@ -32,7 +32,7 @@ class TDaemon{
         int SetFdLimit(int MaxFd);
         int WorkProc();
         int ReloadConfig();
-        int LoadConfig(const char* Filename);
+        virtual int LoadConfig(const char* Filename) = 0;
         static void SelfDebug(int sig, siginfo_t *si, void *ptr);
 };
 
